0328b43d6641cf4e2ea873a645956d0e0ce3ae62
unknown <walefylima@gmail.com>
2024-07-10 01:58:36 -0300

    Gerando

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
-import uvicorn
 import os
-from core.api import api
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+import django
+django.setup()
+
+import uvicorn
+from core.api import api 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(api, host="0.0.0.0", port=port, reload=True)
+    port = int(os.getenv('PORT', 8000))
+    uvicorn.run(api, host="0.0.0.0", port=port)
