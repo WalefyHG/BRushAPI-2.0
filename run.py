d94ccb00821ec96e5fc1d728aadf062f97c0c900
unknown <walefylima@gmail.com>
2024-07-10 01:30:28 -0300

    Gerando coisas

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(api, host="0.0.0.0.", port=port)
+    uvicorn.run(api, host="0.0.0.0", port=port, reload=True)
