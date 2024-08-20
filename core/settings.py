04aac3f5c1cc825cd8b69daa3940e87acf69403b
unknown <walefylima@gmail.com>
2024-08-20 15:58:06 -0300

    Gerando Comentario

--- core/settings.py
+++ core/settings.py
@@ -159,6 +159,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
