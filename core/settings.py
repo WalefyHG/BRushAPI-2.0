a1622b7d14aa10b56b0e148a8e8e60ca8739f3a7
unknown <walefylima@gmail.com>
2024-08-23 13:36:56 -0300

    Reiniciando Settings

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,6 @@ import mimetypes
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -34,6 +33,10 @@ ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if DEBUG:
+    mime = mimetypes.MimeTypes()
+    mime.add_type("text/css", ".css", True)
 
 
 # Application definition
